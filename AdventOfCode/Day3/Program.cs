b11450d67c40870ffc42c01539477d03ed0e9435
Baron <baronalexandre35@gmail.com>
2024-12-03 21:57:06 +0100

    :sparkles: Day 3

--- AdventOfCode/Day3/Program.cs
+++ AdventOfCode/Day3/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace AdventOfCode.Day3;
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode.Day3;
 
 public class Day3
 {
@@ -6,22 +8,59 @@ public class Day3
 
     public Day3()
     {
-        Console.WriteLine("Day3");
+        Console.WriteLine(GetSimpleMultiplies(new(@"mul\((\d+),(\d+)\)")));
+        Console.WriteLine(GetComplexMultiplies(new(@"(do\(\))|(don't\(\))|mul\((\d+),(\d+)\)")));
     }
 
-    private static List<List<int>> GetReports()
+    private static int GetSimpleMultiplies(Regex regex)
     {
-        List<List<int>> reports = [];
+        var result = 0;
 
         if (File.Exists(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, FILEPATH)))
         {
-            foreach (var line in File.ReadLines(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, FILEPATH)))
+            var text = File.ReadAllText(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, FILEPATH));
+
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (Match match in matches)
             {
-                var report = line.Split(" ").Select(int.Parse).ToList();
-                reports.Add(report);
+                int num1 = int.Parse(match.Groups[1].Value);
+                int num2 = int.Parse(match.Groups[2].Value);
+                result += num1 * num2;
+            }
+            return result;
+        }
+        else
+        {
+            throw new Exception("Le fichier n'existe pas.");
+        }
+    }
+
+    private static int GetComplexMultiplies(string regex)
+    {
+        var result = 0;
+
+        if (File.Exists(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, FILEPATH)))
+        {
+            var text = File.ReadAllText(Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, FILEPATH));
+
+            bool actif = true;
+
+            foreach (Match match in Regex.Matches(text, regex))
+            {
+                if (!actif && match.Groups[1].Success)
+                    actif = true;
+                else if (actif && match.Groups[2].Success)
+                    actif = false;
+                else if (match.Groups[3].Success && match.Groups[4].Success && actif)
+                {
+                    int num1 = int.Parse(match.Groups[3].Value);
+                    int num2 = int.Parse(match.Groups[4].Value);
+                    result += num1 * num2;
+                }
             }
 
-            return reports;
+            return result;
         }
         else
         {
