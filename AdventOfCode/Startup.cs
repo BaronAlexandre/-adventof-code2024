da76e12c46d2797c7192b5f5bd225f4a437cc50e
Baron <baronalexandre35@gmail.com>
2024-12-11 00:24:12 +0100

    :wrench: Si mois de décembre je joue le script du jour :)

--- AdventOfCode/Startup.cs
+++ AdventOfCode/Startup.cs
@@ -1,7 +1,11 @@
-Console.WriteLine("Quel jour ?");
-var input = Console.ReadLine();
+int day = DateTime.Now.Day;
 
-string className = $"AdventOfCode.Day{input}.Day{input}";
+if (DateTime.Now.Month != 12)
+{
+    Console.WriteLine("Quel jour ?");
+    day = int.Parse(Console.ReadLine());
+}
+
+string className = $"AdventOfCode.Day{day}.Day{day}";
 
 Activator.CreateInstance(Type.GetType(className));
-
